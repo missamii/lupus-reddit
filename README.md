c65bd72f3c8e92836c9974662c7eb775997eb7df
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2017-07-20 16:58:26 -0400

    ajax call sucessful to the console

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # lupus-reddit
 Project for CraftNY
+
+
+### About Lupus Subreddit
--- js/script.js
+++ js/script.js
@@ -1,19 +1,22 @@
-$(document).ready(function() {
-  console.log("DOM loaded");
-  $.ajax({ //the heart of json
-      url: "https://www.reddit.com/r/ItsNeverLupus/.json",
-      method: "GET",
-      dataType: "json",
-      success: function(res) {
-        // for loop to append data to screen
-      }
-      //create a delete
+window.onload = function() {
+
+  $.ajax({
+      url: 'https://www.reddit.com/r/ItsNeverLupus/.json',
+      type: 'GET',
+      dataType: 'json',
+      // data: {
+      //   param1: 'value1'
+      // }
+    })
+    .done(function(res) {
+      console.log("success", res);
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
 
 
-    },
-    error: function(res) {
-      console.log("Can't retreive data");
-    }
-  });
-
-}); //end ready function
+}; //end onloaded function
