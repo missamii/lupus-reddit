1aa13409637321404cf7683b8f7410b45f471566
Tiffany Lo <tiffany.m.moore.nyc@gmail.com>
2017-07-23 23:03:14 -0400

    issues with appending img

--- js/script.js
+++ js/script.js
@@ -4,26 +4,34 @@ window.onload = function() {
   // var favoriteBtn = getElementById('#favorites');
   // var newdiv = getElementById('#createDiv'); // incase I need to create a new Div
 
-  //heart of data
+
+  // heart of data
   $.ajax({
       url: 'https://www.reddit.com/r/ItsNeverLupus/.json',
       type: 'GET',
       dataType: 'json',
-      // data: {
-      //   param1: 'value1'
-      // }
     })
     .done(function(res) {
       // console.log("success", res);
       var lupusData = res.data.children;
 
-
       console.log("Get the first data obj:", lupusData[0].data);
-      console.log("Got the Title:", lupusData[10].data.title);
-      console.log("Got the Author:", lupusData[10].data.author);
-      console.log("Got the Subreddit:", lupusData[10].data.subreddit_name_prefixed);
+      console.log("Got the Title:", lupusData[0].data.title);
+      console.log("Got the Author:", lupusData[0].data.author);
+      console.log("Got the Subreddit:", lupusData[0].data.subreddit_name_prefixed);
       console.log("Got the IMG:", lupusData[4].data.preview.images[0].source.url);
 
+      var outputTitle = document.getElementById('title');
+      var outputImage = document.getElementsByName('src'); // get back to this one
+      var outputAuthor = document.getElementById('person');
+
+
+      outputTitle.innerHTML = '<h1>' + lupusData[0].data.title + '</h1>';
+      outputImage.attributionHTML = '<img src="' + lupusData[4].data.preview.images[0].source.url + '">';
+      outputAuthor.innerHTML = '<p> <i class="fa fa-user" aria-hidden="true"></i> /u/' + lupusData[2].data.author + '</p>';
+
+
+      console.log("got the IMG", outputImage);
 
 
 
